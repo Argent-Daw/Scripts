efde71f1614a993cc905731ed2eddaa3e70a06c4
Argent-Daw <150301071+Argent-Daw@users.noreply.github.com>
2023-11-15 04:38:41 -0800

    Update Linux_Remote.sh

--- Linux_Remote.sh
+++ Linux_Remote.sh
@@ -18,3 +18,7 @@ sudo systemctl enable --now xrdp
 # Create Firewall Rules
 sudo ufw allow from any to any port 22 proto tcp
 sudo ufw allow from any to any port 3389 proto tcp
+
+# Source https://codefellows.github.io/setup-guide/ops-setup/2-remote-access.html
+
+# End
