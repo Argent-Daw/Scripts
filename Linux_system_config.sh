e95f9bfe8f8646f2a26e92ae582a4e9d414b3dae
Argent-Daw <150301071+Argent-Daw@users.noreply.github.com>
2023-11-15 04:43:10 -0800

    Create Linux_system_config.sh

---
+++ Linux_system_config.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Script Name:                  System Config
+# Author:                       Matthew Earles
+# Date of latest revision:      11/15/2023
+# Purpose:                      A convenient was to set up you system!
+
+# Main
+
+# Deactivate screen locking
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+
+# Set dark mode as default
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+
+# Auto-open gnome-terminal upon login
+mkdir ~/.config/autostart/
+
+touch ~/.config/autostart/gnome-terminal.desktop
+
+sudo cat <<EOL >> ~/.config/autostart/gnome-terminal.desktop
+[Desktop Entry]
+Type=Application
+Exec=gnome-terminal
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[en_NG]=Terminal
+Name=Terminal
+Comment[en_NG]=Start Terminal On Startup
+Comment=Start Terminal On Startup
+EOL
+
+# Set Favorite Apps
+gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'terminal.desktop']"
+gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'files.desktop']"
+gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'virtualbox.desktop']"
+gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'code.desktop']"
+
+# Set Gnome Terminal’s Colors
+
+wget https://raw.githubusercontent.com/codefellows/setup-guide/main/configs/ops/gnome-terminal-profiles.dconf
+dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal-profiles.dconf
+rm gnome-terminal-profiles.dconf
+
+# Source: https://codefellows.github.io/setup-guide/ops-setup/5-system.html
+
+# End
